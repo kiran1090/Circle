6c68e883dbc520943d6b1b859df03aa3c7776de2
kiran_a <iamkiranashok@gmail.com>
2022-12-27 17:17:54 +0530

    constructor Overloading

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Circle {
+import java.awt.geom.Area;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double radius) {
+        radius = r;
+        color = "red";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Area = r * r * Math.PI;
+
+    }
 }
